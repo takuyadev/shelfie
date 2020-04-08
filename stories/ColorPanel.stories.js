9e4fccec880b23e7454502e5b0c76c0a723fcf75
TakuyaToyokawa <takuya.k.toyokawa@gmail.com>
2020-04-08 12:42:47 -0700

    colorPanel Full Draft v1

--- stories/ColorPanel.stories.js
+++ stories/ColorPanel.stories.js
@@ -17,25 +17,49 @@ export const FourPanelLayout = () => (
       height: "500px",
     }}
   >
-    <ColorPanel />
-    <ColorPanel />
-    <ColorPanel />
-    <ColorPanel />
+    <ColorPanel
+      backgroundColor="#7DD0FF"
+      boxShadow="0px 10px 0px #3A799D"
+      text="Winter"
+    />
+    <ColorPanel
+      backgroundColor="#CEFF7D"
+      boxShadow="0px 10px 0px #78AE1E"
+      text="Spring"
+    />
+    <ColorPanel
+      backgroundColor="#FFF27D"
+      boxShadow="0px 10px 0px #BCAB19"
+      text="Summer"
+    />
+    <ColorPanel
+      backgroundColor="#FFA15C"
+      boxShadow="0px 10px 0px #CA5E5E"
+      text="Fall"
+    />
   </div>
 );
 
 export const TwoPanelLayout = () => (
-  <div
-    style={{
-      display: "grid",
-      gridTemplateRows: "1fr",
-      gridTemplateColumns: "1fr 1fr",
-      gridGap: "20px",
-      width: "500px",
-      height: "500px",
-    }}
-  >
-    <ColorPanel />
-    <ColorPanel />
-  </div>
-);
+         <div
+           style={{
+             display: "grid",
+             gridTemplateRows: "1fr",
+             gridTemplateColumns: "1fr 1fr",
+             gridGap: "20px",
+             width: "500px",
+             height: "500px",
+           }}
+         >
+           <ColorPanel
+             backgroundColor="#7DD0FF"
+             boxShadow="0px 10px 0px #3A799D"
+             text="Winter"
+           />
+           <ColorPanel
+             backgroundColor="#FFA15C"
+             boxShadow="0px 10px 0px #CA5E5E"
+             text="Fall"
+           />
+         </div>
+       );
