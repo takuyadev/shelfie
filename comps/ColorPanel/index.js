ea833e9565552bda6984117543a06df68bb92390
TakuyaToyokawa <takuya.k.toyokawa@gmail.com>
2020-04-08 10:57:49 -0700

    init for ColorPanel

---
+++ comps/ColorPanel/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ColorPanel.css';
+
+const FourPanel = ({text, backgroundColor, img}) => {
+    <div className="colorPanels">
+
+    </div>
+}
+FourPanel.defaultProp = {
+    text: "Default",
+    backgroundColor: "#EEE",
+    img: defaultImg
+}
+export default ColorPanel;
---
+++ stories/ColorPanel.stories.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ColorPanel from '../comps/ColorPanel'
+
+export default {
+  title: 'Color Panel',
+};
+
+export const FourPanelLayout = () => {
+  <div>
+    <ColorPanel/>
+  </div>
+
+}
+
+export const TwoPanelLayout = () => {
+  <div>
+
+  </div>
+}
