fcb99670ef91d8747f14dce2c119dce8a9c2ffdc
TakuyaToyokawa <takuya.k.toyokawa@gmail.com>
2020-04-21 11:46:08 -0700

    fixes

--- pages/Solution/index.js
+++ pages/Solution/index.js
@@ -1,30 +1,47 @@
-import React from 'react';
+import React from "react";
 import Link from "next/link";
 import RestartButton from "../../comps/RestartButton";
 import Text from "../../comps/Text";
 import Wave from "../../comps/wave";
-import './solution.css';
-import LogoImage from './LogoDesign.svg'; 
+import "./solution.css";
+import LogoImage from "./LogoDesign.svg";
 
-const Solution = () => <div class="entirePage">
-    <RestartButton></RestartButton>
-    <p>Based on your selections, you have about:</p>
-    <h4>6 Days Left!</h4>
-    <img src={LogoImage} />
-    <br></br>
-    <h6>How Long Does it Last <br></br>Under Best Conditions?</h6>
-    <p class="soultionBox">explanation of how long the food should last under the best conditions</p>
-    <h6>How to Preserve This Food <br></br>Longer?</h6>
-    <p class="soultionBox">tips on how to preserve produce will go here</p>
-    <h6>Misconceptions About <br></br>Expiration Dates</h6>
-    <p class="soultionBox">Educational information will go here</p>
-    <h6>Food Waste</h6>
-    <p class="soultionBox">information about the amount of food is wasted, and encourage the user to make better food purchasing decisions</p>
+const Solution = () => (
+  <div class="app">
+    <div className="nav-button-layout">
+      <p>Based on your selections, you have about:</p>
+      <RestartButton></RestartButton>
+    </div>
+    <main>
+      <h4>6 Days Left!</h4>
+      <img src={LogoImage} />
+      <br></br>
+      <h6>
+        How Long Does it Last <br></br>Under Best Conditions?
+      </h6>
+      <p class="solutionBox">
+        explanation of how long the food should last under the best conditions
+      </p>
+      <h6>
+        How to Preserve This Food <br></br>Longer?
+      </h6>
+      <p class="solutionBox">tips on how to preserve produce will go here</p>
+      <h6>
+        Misconceptions About <br></br>Expiration Dates
+      </h6>
+      <p class="solutionBox">Educational information will go here</p>
+      <h6>Food Waste</h6>
+      <p class="solutionBox">
+        information about the amount of food is wasted, and encourage the user
+        to make better food purchasing decisions
+      </p>
+    </main>
     <Wave></Wave>
-</div>
+  </div>
+);
 
 Text.defaultProps = {
-    color: '#322D2D'
-}
+  color: "#322D2D",
+};
 
-export default Solution; 
+export default Solution;
