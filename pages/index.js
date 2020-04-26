0df423adba162d3e27c3eb28c1caaec1a05acfdb
TakuyaToyokawa <takuya.k.toyokawa@gmail.com>
2020-04-26 16:17:53 -0700

    fix for prototype testing

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
 import Link from "next/link";
 import "./index.css";
-import {data, ChangeData} from '../data';
 
 const Index = () => (
   <div id="mainapp">
-    <Link href="/Fridge"><a>Index</a></Link>
+    <Link href="/Homepage"><a>Index</a></Link>
   </div>
 );
 
