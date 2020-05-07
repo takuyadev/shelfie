87e7b4b43e68b43f80b2b255e0022549a1694fab
TakuyaToyokawa <takuya.k.toyokawa@gmail.com>
2020-05-07 02:05:19 -0700

    changed text to match with current information

--- pages/Solution/index.js
+++ pages/Solution/index.js
@@ -8,39 +8,42 @@ import "./solution.css";
 import LogoImage from "./LogoDesign.svg";
 import { data, ChangeData, SolutionText } from "../../data";
 import Textbox from "../../comps/Textbox";
+
 console.log(
   SolutionText.Fridge[data.Fridge].Seasons[data.Seasons].FruitorVegetable[
     data.FruitorVegetable
   ].Option[data.option]
 );
+
 console.log(data);
 
 const Solution = () => {
   const [bestConditionText, setBestConditionText] = useState(
     SolutionText.Fridge[data.Fridge].Seasons[data.Seasons].FruitorVegetable[
       data.FruitorVegetable
-    ].Option[data.Option].Text
+    ].Option[data.Option].bestCondition
   );
+  const [PreserveText, setPreserveText] = useState(
+    SolutionText.Fridge[data.Fridge].Seasons[data.Seasons].FruitorVegetable[
+      data.FruitorVegetable
+    ].Option[data.Option].Preserve
+  );
+  const [DaysText, setDaysText] = useState(
+    SolutionText.Fridge[data.Fridge].Seasons[data.Seasons].FruitorVegetable[
+      data.FruitorVegetable
+    ].Option[data.Option].Days
+  );
+  
   return (
     <div className="app">
       <div className="navBar" id="anchorTop">
         <RestartButton></RestartButton>
       </div>
       <main>
-        <h4>
-          {
-            SolutionText.Fridge[data.Fridge].Seasons[data.Seasons]
-              .FruitorVegetable[data.FruitorVegetable].Option[data.Option].Days
-          }{" "}
-          Days Left!
-        </h4>
+        <h4>{DaysText} Left!</h4>
         <p>
-          By all estimates, this food will last about{" "}
-          {
-            SolutionText.Fridge[data.Fridge].Seasons[data.Seasons]
-              .FruitorVegetable[data.FruitorVegetable].Option[data.Option].Days
-          }{" "}
-          days under your selected conditions{" "}
+          By all estimates, this food will last about {DaysText} under your
+          selected conditions
         </p>
         <img src={LogoImage} className="BananaMan" />
         <br></br>
@@ -48,10 +51,13 @@ const Solution = () => {
           header="How Long Does it Last Under Best Conditions?"
           text={bestConditionText}
         />
-        <Textbox header="How to Preserve This Food Longer?" />
+        <Textbox
+          header="How to Preserve This Food Longer?"
+          text={PreserveText}
+        />
         <Textbox header="Misconceptions About Expiration Dates" />
         <Textbox header="How to reduce Food Waste" />
-        <ConfirmButton link="#anchorTop" text="Go back to the top!"/>
+        <ConfirmButton link="#anchorTop" text="Go back to the top!" />
       </main>
       <Wave backgroundColor={data.Wave} />
     </div>
